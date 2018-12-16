2ac8e0e6e3a15bd4d01a41af937c801e3df7416f
Your Name <you@example.com>
2018-12-16 12:18:07 +0530

    added post and submit button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container } from 'reactstrap';
+
+import PostViewer from './PostViewer';
+import PostEditor from './PostEditor';
 
 class App extends Component {
+  state = {
+    editing: null,
+  };
+
   render() {
+    const { editing } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container fluid>
+        <Button
+          className="my-2"
+          color="primary"
+          onClick={() => this.setState({ editing: {} })}
+        >
+          New Post
+        </Button>
+        <PostViewer
+          canEdit={() => true}
+          onEdit={(post) => this.setState({ editing: post })}
+        />
+        {editing && (
+          <PostEditor
+            post={editing}
+            onClose={() => this.setState({ editing: null })}
+          />
+        )}
+      </Container>
     );
   }
 }
